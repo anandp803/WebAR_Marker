02fce3fd318339131d6e61970ff4894d2edf8282
Anand <anand.2.prakash@atos.net>
2024-03-15 23:54:42 +0530

    no message

--- Scripts/WebARMaeker.js
+++ Scripts/WebARMaeker.js
@@ -32,7 +32,7 @@ class Demo {
         };
 
         var ANote0Video = BABYLON.MeshBuilder.CreatePlane("plane", planeOpts, scene);
-        ANote0Video.rotationQuaternion = new BABYLON.Quaternion(new BABYLON.Vector3(0,90,0));
+        ANote0Video.rotationQuaternion = new BABYLON.Quaternion(new BABYLON.Vector3(0,0,0));
         ANote0Video.parent = root;
         ANote0Video.setPivotPoint(new BABYLON.Vector3(0, 0, 0));
         //var vidPos = (new BABYLON.Vector3(0,0,0.1));
